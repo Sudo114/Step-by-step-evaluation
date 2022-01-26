e737634cdf95ebc09910fa77bc9647456a61b453
Sudo114 <98070105+Sudo114@users.noreply.github.com>
2022-01-26 07:05:18 +0530

    Create Evaluation.c

---
+++ Evaluation.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int x = 2;
+    int y = 3;
+    int z = 3;
+    int k = 1;
+
+    printf("**** THIS IS STEP BY STEP EVALUATION OF 3*x/y-z+k *****\n");
+
+    printf("WHERE ------->\n");
+    printf("-----> x=2 <----\n");
+    printf("-----> y=3 <----\n");
+    printf("-----> z=3 <----\n");
+    printf("-----> k=1 <---- \n");
+
+    printf("Evaluation Start Here \n");
+    printf("- - - - \n");
+    printf("- - - - \n");
+    printf("- - - - \n");
+
+    printf("3*x/y-z+k\n");
+    printf("3*2/3-3+1\n");
+    printf("6/3-3+1\n");
+    printf("3-3+1\n");
+    printf("0-1\n");
+    printf("1\n");
+
+    return 0;
+}
